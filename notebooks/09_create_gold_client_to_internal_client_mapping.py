de5dd1911efc6b836aed8615fd59c40009503f92
FatCatAnalytics <35469989+FatCatAnalytics@users.noreply.github.com>
2026-06-22 21:45:48 +0100

    Improve internal client fallback and normalized LEI matching

--- notebooks/09_create_gold_client_to_internal_client_mapping.py
+++ notebooks/09_create_gold_client_to_internal_client_mapping.py
@@ -8,6 +8,10 @@ CREATE OR REPLACE TABLE {catalog}.{schema}.gold_client_to_internal_client_mappin
 WITH hierarchy AS (
     SELECT
         *,
+        nullif(upper(trim(matched_lei)), '') AS matched_lei_norm,
+        nullif(upper(trim(direct_parent_lei)), '') AS direct_parent_lei_norm,
+        nullif(upper(trim(ultimate_parent_lei)), '') AS ultimate_parent_lei_norm,
+        trim(regexp_replace(regexp_replace(regexp_replace(regexp_replace(lower(coalesce(submitted_name, '')), '&', ' and '), '[^a-z0-9 ]', ' '), '\\b(inc|incorporated|ltd|limited|llc|plc|corp|corporation|co|company|ag|sa|bv|nv|gmbh|pte|pty|holdings|holding|group)\\b', ' '), ' +', ' ')) AS submitted_clean_name,
         trim(regexp_replace(regexp_replace(regexp_replace(regexp_replace(lower(coalesce(matched_name, '')), '&', ' and '), '[^a-z0-9 ]', ' '), '\\b(inc|incorporated|ltd|limited|llc|plc|corp|corporation|co|company|ag|sa|bv|nv|gmbh|pte|pty|holdings|holding|group)\\b', ' '), ' +', ' ')) AS matched_clean_name,
         trim(regexp_replace(regexp_replace(regexp_replace(regexp_replace(lower(coalesce(direct_parent_name, '')), '&', ' and '), '[^a-z0-9 ]', ' '), '\\b(inc|incorporated|ltd|limited|llc|plc|corp|corporation|co|company|ag|sa|bv|nv|gmbh|pte|pty|holdings|holding|group)\\b', ' '), ' +', ' ')) AS direct_parent_clean_name,
         trim(regexp_replace(regexp_replace(regexp_replace(regexp_replace(lower(coalesce(ultimate_parent_name, '')), '&', ' and '), '[^a-z0-9 ]', ' '), '\\b(inc|incorporated|ltd|limited|llc|plc|corp|corporation|co|company|ag|sa|bv|nv|gmbh|pte|pty|holdings|holding|group)\\b', ' '), ' +', ' ')) AS ultimate_parent_clean_name
@@ -16,6 +20,8 @@ WITH hierarchy AS (
 hierarchy_blocked AS (
     SELECT
         *,
+        split(submitted_clean_name, ' ')[0] AS submitted_first_token,
+        substring(submitted_clean_name, 1, 4) AS submitted_prefix_4,
         split(matched_clean_name, ' ')[0] AS matched_first_token,
         substring(matched_clean_name, 1, 4) AS matched_prefix_4,
         split(direct_parent_clean_name, ' ')[0] AS direct_parent_first_token,
@@ -32,8 +38,8 @@ internal_client AS (
         lei,
         internal_rating,
         business_owner,
-        trim(regexp_replace(regexp_replace(regexp_replace(regexp_replace(lower(coalesce(legal_name, '')), '&', ' and '), '[^a-z0-9 ]', ' '), '\\b(inc|incorporated|ltd|limited|llc|plc|corp|corporation|co|company|ag|sa|bv|nv|gmbh|pte|pty|holdings|holding|group)\\b', ' '), ' +', ' ')) AS internal_clean_name
-    FROM {catalog}.{schema}.internal_client_entities_raw
+        clean_name AS internal_clean_name
+    FROM {catalog}.{schema}.internal_client_entities_clean
 ),
 internal_client_blocked AS (
     SELECT
@@ -46,45 +52,64 @@ lei_candidates AS (
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
            'DIRECT_LEI_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 1 AS mapping_priority, 1.00 AS internal_mapping_score
     FROM hierarchy_blocked h
-    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.matched_lei
+    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.matched_lei_norm
 
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
            'DIRECT_PARENT_LEI_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 2 AS mapping_priority, 1.00 AS internal_mapping_score
     FROM hierarchy_blocked h
-    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.direct_parent_lei
+    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.direct_parent_lei_norm
 
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
            'ULTIMATE_PARENT_LEI_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 3 AS mapping_priority, 1.00 AS internal_mapping_score
     FROM hierarchy_blocked h
-    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.ultimate_parent_lei
+    INNER JOIN internal_client_blocked i ON i.lei_norm IS NOT NULL AND i.lei_norm = h.ultimate_parent_lei_norm
 ),
 name_candidates AS (
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'EXACT_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 4 AS mapping_priority, 0.92 AS internal_mapping_score
+           'SUBMITTED_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 4 AS mapping_priority, 0.94 AS internal_mapping_score
+    FROM hierarchy_blocked h
+    INNER JOIN internal_client_blocked i ON h.submitted_clean_name <> '' AND i.internal_clean_name <> '' AND h.submitted_clean_name = i.internal_clean_name
+
+    UNION ALL
+
+    SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
+           'GLEIF_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 5 AS mapping_priority, 0.92 AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i ON h.matched_clean_name <> '' AND i.internal_clean_name <> '' AND h.matched_clean_name = i.internal_clean_name
 
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'DIRECT_PARENT_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 5 AS mapping_priority, 0.90 AS internal_mapping_score
+           'DIRECT_PARENT_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 6 AS mapping_priority, 0.90 AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i ON h.direct_parent_clean_name <> '' AND i.internal_clean_name <> '' AND h.direct_parent_clean_name = i.internal_clean_name
 
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'ULTIMATE_PARENT_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 6 AS mapping_priority, 0.88 AS internal_mapping_score
+           'ULTIMATE_PARENT_NORMALIZED_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 7 AS mapping_priority, 0.88 AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i ON h.ultimate_parent_clean_name <> '' AND i.internal_clean_name <> '' AND h.ultimate_parent_clean_name = i.internal_clean_name
 ),
 fuzzy_candidates AS (
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 7 AS mapping_priority,
+           'SUBMITTED_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 8 AS mapping_priority,
+           round(1.0 - (levenshtein(h.submitted_clean_name, i.internal_clean_name) / greatest(length(h.submitted_clean_name), length(i.internal_clean_name))), 4) AS internal_mapping_score
+    FROM hierarchy_blocked h
+    INNER JOIN internal_client_blocked i
+        ON h.submitted_clean_name <> ''
+       AND i.internal_clean_name <> ''
+       AND (h.submitted_first_token = i.internal_first_token OR h.submitted_prefix_4 = i.internal_prefix_4)
+       AND 1.0 - (levenshtein(h.submitted_clean_name, i.internal_clean_name) / greatest(length(h.submitted_clean_name), length(i.internal_clean_name))) >= 0.78
+
+    UNION ALL
+
+    SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
+           'GLEIF_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 9 AS mapping_priority,
            round(1.0 - (levenshtein(h.matched_clean_name, i.internal_clean_name) / greatest(length(h.matched_clean_name), length(i.internal_clean_name))), 4) AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i
@@ -96,7 +121,7 @@ fuzzy_candidates AS (
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'DIRECT_PARENT_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 8 AS mapping_priority,
+           'DIRECT_PARENT_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 10 AS mapping_priority,
            round(1.0 - (levenshtein(h.direct_parent_clean_name, i.internal_clean_name) / greatest(length(h.direct_parent_clean_name), length(i.internal_clean_name))), 4) AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i
@@ -108,7 +133,7 @@ fuzzy_candidates AS (
     UNION ALL
 
     SELECT h.*, i.internal_entity_id, i.legal_name AS internal_client_name, i.lei AS internal_client_lei, i.internal_rating, i.business_owner,
-           'ULTIMATE_PARENT_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 9 AS mapping_priority,
+           'ULTIMATE_PARENT_FUZZY_NAME_MATCH_TO_INTERNAL_CLIENT' AS internal_mapping_type, 11 AS mapping_priority,
            round(1.0 - (levenshtein(h.ultimate_parent_clean_name, i.internal_clean_name) / greatest(length(h.ultimate_parent_clean_name), length(i.internal_clean_name))), 4) AS internal_mapping_score
     FROM hierarchy_blocked h
     INNER JOIN internal_client_blocked i
